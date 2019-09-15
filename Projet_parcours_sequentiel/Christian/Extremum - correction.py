fa0fb6b10cf71e268efefa093f364fd6fcea1c3d
lau98709 <lau@mail.pf>
2019-09-14 20:43:53 -1000

    Finalisation de la partie recherche d'extremums
    Finalisation des documents sur la partie recherches d'extremums.

--- Projet_parcours_sequentiel/Christian/Extremum - correction.py
+++ Projet_parcours_sequentiel/Christian/Extremum - correction.py
@@ -11,6 +11,8 @@ import math
 import time
 import random
 
+#------ Les variables globale ---------
+
 size = 300
 data = None
 
@@ -23,14 +25,20 @@ index = 0
 bias = 0.8
 funct = None
 
+#---------------------------- ---------
 
+
+#  Affichage des données
 def DrawData( canvas, data ):
     global i_max, maximal, i_min, minimal, index
     
     if data == None:
         print("data == None")
         return
+
     w = int(canvas['width']); h = int(canvas['height'])
+    
+    # Dessin des points et les lignes les reliant
     n = len(data)
     size = 1
     for i in range(0,n):
@@ -40,17 +48,23 @@ def DrawData( canvas, data ):
         if (i > 0): canvas.create_line(lx, ly, x, y, fill="blue")
         lx = x; ly = y
 
+    # Dessin de la ligne de balayage
     if (index >= 0) or (index < n-1):
         x = index*w/n;
         canvas.create_line(x, 0, x, h, width=2, fill="red")
 
+    # Dessin des points du maximum et du minimum    
     x = i_max*w/n; y = h/2 + maximal*h; size = 5
     canvas.create_oval(x-size,y-size,x+size,y+size,width=1,fill="#80FF80")
     x = i_min*w/n; y = h/2 + minimal*h; size = 5
     canvas.create_oval(x-size,y-size,x+size,y+size,width=1,fill="#FF8080")
 
 
+# Création des données
 def GenData( n ):
+    # n = nombre d'échantillons
+    # Retourne la liste des données créée
+    # Cette fonction fait appel à la fonction pointée par la variable "funct"
     global funct
     
     data = list()
@@ -59,13 +73,17 @@ def GenData( n ):
     return data
 
 
+# Fonction génératrice des données
 def SinusSinus( x ):
+    # x = valeur entrée de la fonction, compris entre 0 et 1
+    # Retourne la valeur correspondante à x
     global bias
 
     f = 2*math.pi*x;
     return 0.3*math.sin(10*f)*math.cos(2*f)*math.exp(-7*(x-bias)**2)
 
 
+# Redessiner le graphique
 def ReDraw():
     global canvas, data
     
@@ -75,14 +93,21 @@ def ReDraw():
     fenetre.update()                # Mise à jour de la fenêtre
 
 
+# Fonction de recherche des extremum
 def Search():
+    # Cette fonction met à jour les valeurs des extremums
+    # représentés par les variables "maximal", "minimal".
+    # Met aussi à jour les variables "i_max" et "i_min"
+    # correspondant à l'index des extremums dans la liste des données
     global i_max, maximal, i_min, minimal, data, index
     
+    # Initialisation des extremums à la 1ère valeur
     i_max = 0
     maximal = data[0]
     i_min = 0
     minimal = data[0]
 
+    # Boucle de balayage de la liste
     for index in range(len(data)):
 
         if (data[index] > maximal):
@@ -92,10 +117,12 @@ def Search():
             minimal = data[index]
             i_min = index
 
-        ReDraw()
+        ReDraw()    # Mise à jour graphique
 
 
+# Action déclencher par le menu "Recherche extremum"
 def ActionMenu():
+    # Remise à zéro de la recherche
     global bias, data, size, funct
     bias = 0.3+random.random()*0.6
     data = GenData(size)
@@ -104,9 +131,9 @@ def ActionMenu():
 
 # Création de la fenêtre
 fenetre = Tk()
-fenetre.title("Labyrinthe")
+fenetre.title("Recherche d'extremums")
 
-# Création d'un menu avec une commande pour recommencer
+# Création d'un menu avec une commande pour remise à zéro
 menubar = Menu(fenetre)
 menubar.add_command(label="Recherche extremum", command=ActionMenu)
 fenetre.config(menu=menubar)
@@ -115,9 +142,11 @@ fenetre.config(menu=menubar)
 canvas = Canvas(fenetre,width=600,height=480,background="white")
 canvas.pack()
 
+# Génération des données
 funct = SinusSinus
 data = GenData(size)
 
+# Recherche des extremums
 Search()
 
 fenetre.mainloop()
